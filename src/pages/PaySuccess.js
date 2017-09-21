95fdc03f7486c89478a9e4b759b6231aca180251
Millie <millie@deshpro.com>
2017-09-21 17:32:57 +0800

    修改

--- src/pages/PaySuccess.js
+++ src/pages/PaySuccess.js
@@ -2,11 +2,18 @@ import React, {Component} from 'react';
 import '../styles/Pay.css'
 
 export default class PaySuccess extends Component {
-    render(){
-        return(
-            <p>
+    componentDidMount() {
+        setTimeout(function () {
+            window.postMessage('pay-success', window.location.href)
+        }, 100)
+
+    }
+
+
+    render() {
+        return ( <p>
                 pay-success works!
-            </p>
+            </p >
         )
     }
 }
