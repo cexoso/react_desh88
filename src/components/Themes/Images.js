fa3608ac4e5a57f0d5b4b07c288b5fcaa055061f
Millie <millie@deshpro.com>
2017-11-06 18:09:22 +0800

    视频页面新增列表

--- src/components/Themes/Images.js
+++ src/components/Themes/Images.js
@@ -1,6 +1,7 @@
 
 const images = {
     returnIcon: require('../../assets/images/ReturnIcon@2x.png'),
-    more: require('../../assets/images/more.png')
+    more: require('../../assets/images/more.png'),
+    videoControl: require('../../assets/images/videoControl.png'),
 }
 export default images
--- src/pages/VideoInfo.js
+++ src/pages/VideoInfo.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {getVideoInfo, getVideoGroup,setLang} from '../service/RaceDao';
+import {getVideoInfo, getVideoGroup, setLang} from '../service/RaceDao';
 import {weiXinShare, isEmptyObject} from '../service/utils';
 import {default_img} from '../components/constant';
 import MarkDown from '../components/MarkDown';
@@ -7,11 +7,15 @@ import Footer from '../components/Footer';
 import '../styles/Video.css';
 import I18n from '../service/I18n';
 import {Images} from '../components/Themes';
+import ReactSwipe from 'react-swipes'
 
 export default class VideoInfo extends Component {
     state = {
         data: {},
-        videoGroup:{}
+        videoGroup: {},
+        coverLink:'',
+        videoLink:'',
+        changedDes:''
     };
 
     componentDidMount() {
@@ -31,10 +35,14 @@ export default class VideoInfo extends Component {
 
         getVideoInfo(body, data => {
             console.log('VideoInfo', data)
+
             this.setState({
-                data: data
+                data: data,
+                coverLink:data.cover_link,
+                videoLink:data.video_link,
+                changedDes:data.description
             });
-            const {name,cover_link,description} = data;
+            const {name, cover_link, description} = data;
             document.title = name;
 
             //微信二次分享
@@ -44,51 +52,83 @@ export default class VideoInfo extends Component {
                 title: name,
                 desc: description,//分享描述
                 link: window.location.href, // 分享链接，该链接域名必须与当前企业的可信域名一致
-                imgUrl: isEmptyObject(cover_link)?default_img:cover_link, // 分享图标
+                imgUrl: isEmptyObject(cover_link) ? default_img : cover_link, // 分享图标
                 type: "", // 分享类型,music、video或link，不填默认为link
                 dataUrl: "", // 如果type是music或video，则要提供数据链接，默认为空
             }
             const url = {url: window.location.href};
-            console.log("message:",message);
-            weiXinShare(url,message);
+            console.log("message:", message);
+            weiXinShare(url, message);
         }, err => {
 
         });
 
-    }
+    };
 
     render() {
-        if(isEmptyObject(this.state.data)) {
+        if (isEmptyObject(this.state.data)) {
             return <div></div>;
         }
-        const {description, video_link ,cover_link} = this.state.data;
 
         return (
             <div className="videoInfo">
-                <video ref="myVideo" controls="true"  poster={cover_link}>
-                    <source src={video_link} type="video/mp4"/>
-                    {/*<source src={video_link} type="video/ogg"/>*/}
-                    {/*<source src={video_link} type="video/webm"/>*/}
-                    <object data={video_link}>
-                        <embed src={video_link}/>
-                    </object>
-                </video>
-                <div className="videoTop">
-                    <span>{this.state.data.name}</span>
+                <div className="videoInfoTop">
+                    <video autoPlay="autoPlay" controls="controls"
+                           poster={this.state.coverLink}>
+                        <source style={{width: '100%'}}
+                                src={this.state.videoLink}
+                                type="video/mp4"/>
+                        <object data={this.state.videoLink}>
+                            <embed src={this.state.videoLink}/>
+                        </object>
+                    </video>
+                    <div className="videoTop">
+                        <span>{this.state.data.name}</span>
+                    </div>
                 </div>
+
 
                 <div className="videoGroup">
                     <div className="videoGroupTop">
                         <span className="videoGroupName">{this.state.data.group_name}</span>
-                        <div style={{flex:1}}/>
+                        <div style={{flex: 1}}/>
                         <span className="videoGroupTxt">{I18n.t('more')}</span>
                         <img className="videoGroupImg" src={Images.more} alt=""/>
                     </div>
-                    {this.state.videoGroup.items.map((item)=>{
 
-                    })}
+
+                    <div className="card-swipe" >
+                        <div className="card-slide">
+                            {this.state.videoGroup.items.map((item,index)=>{
+                                return(
+                                    <div key={index} className="video-slide" onClick={()=>{
+                                        this.setState({
+                                            changedDes:item.description,
+                                            coverLink:item.cover_link,
+                                            videoLink:item.video_link
+                                        })
+                                    }}>
+                                        <div className="videoImgs" style={{backgroundImage:`url(${item.cover_link})`,backgroundSize:'100% 100%'}}>
+                                            <div className="videoImg">
+                                                <img src={Images.videoControl} alt=""/>
+                                            </div>
+                                            <div className="videoSpan">
+                                                <span>{item.video_duration}dd</span>
+                                            </div>
+
+                                        </div>
+                                        <div className="videoTxt">
+                                            <span>yyyyyttbfvsfgsacawrcsdbqwzsaddsfsfdsfdasdsdsdsdsdsdssfdsfgsdgdhchfghf</span>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+
                 </div>
-                <MarkDown description={description}/>
+                <MarkDown description={this.state.changedDes}/>
+                <div style={{height:60}}/>
                 <Footer/>
             </div>
         );
