bd894b576a59a9f37b8c439171ac9f0799bea10e
Millie <millie@deshpro.com>
2017-11-13 18:01:17 +0800

    markDown样式修改

--- src/components/MarkDown.js
+++ src/components/MarkDown.js
@@ -1,21 +1,29 @@
 import React, {Component} from 'react';
 import markdown from 'marked';
 import './css/MarkDown.css';
+var renderer = new markdown.Renderer();
 
+renderer.paragraph = function (text) {
+    return '<p>' + text + '</p>'+'</br>';
+};
 
 export default class MarkDown extends Component {
 
 
     desc = (description) => {
-        var des = markdown(description)
+
+        var des = markdown(description, {renderer: renderer});
         return {__html: des}
     }
 
 
-    render(){
+    render() {
         const {description} =this.props;
         return (
-            <div className="introduceGame" dangerouslySetInnerHTML={this.desc(description)}></div>
+            <div style={{width:'100%',height:'100%',backgroundColor:'#FFFFFF',paddingTop:20}}>
+                <div className="introduceGame" dangerouslySetInnerHTML={this.desc(description)}></div>
+            </div>
+
         );
     }
 }
