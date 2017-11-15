d0cd7c78f23f77b9212ccc114f4cb133228fd5b1
Millie <millie@deshpro.com>
2017-11-15 15:33:00 +0800

    Images

--- src/components/Themes/Images.js
+++ src/components/Themes/Images.js
@@ -19,6 +19,8 @@ const images = {
     is: require('../../assets/images/is>.png'),
     download_close: require('../../assets/mall/download_close.png'),
     right: require('../../assets/mall/right.png'),
-    puke: require('../../assets/mall/puke.png')
+    puke: require('../../assets/mall/puke.png'),
+    load_ios: require('../../assets/images/load-ios.png'),
+    load_android: require('../../assets/images/android-load.png'),
 }
 export default images
