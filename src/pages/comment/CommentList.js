a4c0278c2bbbda9f5ed22fb8568846a3a7c6f7e0
lorne <2270333671@qq.com>
2017-12-29 16:37:33 +0800

    发布

--- src/pages/comment/CommentList.js
+++ src/pages/comment/CommentList.js
@@ -261,6 +261,7 @@ export default class CommentList extends Component {
         });
     };
 
+
     toDynanic = (item) => {
         postClick(JSON.stringify({
             route: PostRoute.ClickAvatar,
