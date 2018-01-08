927ab27e6795edc1b2ed15e6c595ca1044fafb0e
Millie <millie@deshpro.com>
2018-01-08 11:50:32 +0800

    支付帮助界面

--- src/pages/pay/PayHelpPage.js
+++ src/pages/pay/PayHelpPage.js
@@ -3,6 +3,8 @@ import {Colors} from '../../components';
 import I18n from '../../service/I18n';
 import {MarkDown, Images,Drawer} from '../../components';
 import {isEmptyObject} from '../../service/utils';
+import '../../styles/payHelpPage.css';
+import { Button} from 'antd-mobile';
 
 export default class PayHelpPage extends Component {
 
@@ -10,18 +12,22 @@ export default class PayHelpPage extends Component {
     render() {
         return (
             <div className="helpPage">
-                <div className="desView weChat">
+                <Button className="desView weChat"  onClick={()=>{
+
+                }}>
                     <img className="image" src="" alt=""/>
                     <span>{I18n.t('pay_limit_weChat')}</span>
                     <div className="separated"/>
-                    <img  className="popImg" src="" alt=""/>
-                </div>
-                <div className="desView yinlian">
+                    <img  className="popImg" src="/static/images/is>.png" alt=""/>
+                </Button>
+                <Button className="desView yinlian" onClick={()=>{
+                    this.props.history.push("/pay/description")
+                }}>
                     <img className="image2" src="" alt=""/>
                     <span>{I18n.t('pay_limit_yinlian')}</span>
                     <div className="separated"/>
-                    <img className="popImg" src="" alt=""/>
-                </div>
+                    <img className="popImg" src="/static/images/is>.png" alt=""/>
+                </Button>
 
                 <div className="desInfo">
                     <span className="count_beyond">
