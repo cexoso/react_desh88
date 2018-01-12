d844aa8b9fb1b7d84745e7b8dfb187de743dc8f9
lorne <2270333671@qq.com>
2018-01-12 15:22:19 +0800

    添加首页导航

---
+++ src/components/TopNav.js
@@ -0,0 +1,26 @@
+/**
+ * Created by lorne on 2018/1/12
+ * Function:
+ * Desc:
+ */
+
+import React, {PureComponent} from 'react';
+import '../styles/footer.css';
+
+export default class TopNav extends PureComponent {
+
+    render() {
+        return <div className="top-bar">
+            <a href={'https://www.deshpro.com/'}>
+                <img src="/static/images/home_bar.png" alt="PokerPro" height={25}/>
+            </a>
+
+            <div style={{flex: 1}}>
+                <img src="/static/images/footerImg.png" alt="PokerPro"
+                     height={25}/>
+            </div>
+
+            <div/>
+        </div>
+    }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -6,8 +6,9 @@ import Carousel from './carousel';
 import {List, Button, WhiteSpace, InputItem,Flex,Modal,Text,ListView} from 'antd-mobile';
 import BaseComponent from './BaseComponent';
 import VideoPlayer from './VideoPlayer';
+import TopNav from './TopNav';
 
 export {
     MarkDown, Footer, Images, Colors, Carousel, List, Button, WhiteSpace,
-    InputItem,Flex,Modal,BaseComponent,Text,ListView,VideoPlayer
+    InputItem,Flex,Modal,BaseComponent,Text,ListView,VideoPlayer,TopNav
 }
