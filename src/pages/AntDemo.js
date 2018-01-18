18afbf1fc0269a140a10b61a078df4ac22ceca24
Millie <millie@deshpro.com>
2018-01-18 18:09:04 +0800

    ant牌桌测试

--- src/pages/AntDemo.js
+++ src/pages/AntDemo.js
@@ -7,78 +7,103 @@ import React, {PureComponent} from 'react';
 import {Flex, Button, WhiteSpace, Carousel} from 'antd-mobile';
 import CommentList from './comment/CommentList';
 import {VideoPlayer} from '../components';
+import classnames from 'classnames';
 import '../App.css';
-
+var count = 0;
 export default class AntDemo extends PureComponent {
+    state={
+        show:false
+    };
 
-    state = {
-        data: ['1', '2', '3'],
-        imgHeight: 176,
-        slideIndex: 0,
-    }
+    componentDidMount() {
+        window.onload=()=>{
+            this.pokerScroll()
+        }
+    };
+    pokerScroll=()=>{
+        let pokerBottoms = document.getElementById('pokerBottom').getElementsByTagName('div');
+        let pokerTops = document.getElementById('pokerTop').getElementsByTagName('div');
+        let pokerLefts = document.getElementById('pokerLeft').getElementsByTagName('div');
+        let pokerRights = document.getElementById('pokerRight').getElementsByTagName('div');
+        let pokers = [];
+        pokers = pokers.concat(pokerTops,pokerRights,pokerBottoms,pokerLefts);
+        for (var i=0; i < pokers.length; i++) {
+            this.setAction(pokers[i],i)
+        }
+    };
 
     render() {
-        const videoJsOptions = {
 
-            autoplay: false,
-            controls: true,
-            poster: 'https://cdn-upyun.deshpro.com/uploads/info/image/82/preview_%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20170724151105.png',
-            sources: [{
-                src: 'http://deshpro-test-video.ufile.ucloud.com.cn/race01.mp4',
-                type: 'video/mp4'
-            }]
-        };
-        return <div style={styles.container}>
+        return (
+            <div className="ant_page">
+                <div className="table">
 
-            <Carousel className="space-carousel"
-                      frameOverflow="visible"
-                      cellSpacing={10}
-                      slideWidth={0.8}
-                      autoplay
-                      infinite
-                      beforeChange={(from, to) => console.log(`slide from ${from} to ${to}`)}
-                      afterChange={index => this.setState({slideIndex: index})}
-            >
-                {this.state.data.map((val, index) => (
-                    <a
-                        key={val}
-                        href="http://www.alipay.com"
-                        style={{
-                            display: 'block',
-                            position: 'relative',
-                            top: this.state.slideIndex === index ? -10 : 0,
-                            height: this.state.imgHeight,
-                            boxShadow: '2px 1px 1px rgba(0, 0, 0, 0.2)',
-                        }}
-                    >
-                        <img
-                            src={`https://zos.alipayobjects.com/rmsportal/${val}.png`}
-                            alt=""
-                            style={{width: '100%', verticalAlign: 'top'}}
-                            onLoad={() => {
-                                // fire window resize event to change height
-                                window.dispatchEvent(new Event('resize'));
-                                this.setState({imgHeight: 'auto'});
-                            }}
-                        />
-                    </a>
-                ))}
-            </Carousel>
+                </div>
+
+                <div className="pokerTop"  id="pokerTop">
+                    {this.pokerTop('poker')}
+                    {this.pokerTop('poker poker2')}
+                    {this.pokerTop('poker poker3')}
+                </div>
+
+                <div className="pokerBottom"  id="pokerBottom">
+                    {this.pokerBottom('poker ')}
+                    {this.pokerBottom('poker poker2')}
+                    {this.pokerBottom('poker poker3')}
+                </div>
+
+                <div className="pokerLeft"  id="pokerLeft">
+                    {this.pokerLeft('poker')}
+                    {this.pokerLeft('poker pokerLeft2')}
+                </div>
+
+                <div className="pokerRight" id="pokerRight">
+                    {this.pokeRight('poker')}
+                    {this.pokeRight('poker pokerLeft2')}
+                </div>
+            </div>
+        )
+    }
 
 
+    pokerBottom = (poker_class) => {
+        return <div className={classnames(poker_class)}>
+            <div className="select select1" id="Bottom1">弃牌</div>
+            <div className="select select2"  id="Bottom2">跟注</div>
+            <div className="select select3"  id="Bottom3">加注</div>
         </div>
+    };
+    pokerTop = (poker_class) => {
+        return <div className={classnames(poker_class)}>
+            <div className="select select1" id="Top1">加注</div>
+            <div className="select select2" id="Top2">跟注</div>
+            <div className="select select3" id="Top3">弃牌</div>
+        </div>
+    };
+    pokerLeft = (poker_class) => {
+        return <div className={classnames(poker_class)}>
+            <div className="select select1" id="Left1">加注</div>
+            <div className="select select2" id="Left1">跟注</div>
+            <div className="select select3" id="Left1">弃牌</div>
+        </div>
+    };
+    pokeRight = (poker_class) => {
+        return <div className={classnames(poker_class)}>
+            <div className="select select1" id="Right1">加注</div>
+            <div className="select select2" id="Right1">跟注</div>
+            <div className="select select3" id="Right1">弃牌</div>
+        </div>
+    };
+
+
+    setAction = (poker,index) => {
+        setInterval(() => {
+            console.log(`打印数字：${count++}`)
+            if(index === 0){
+                count=1
+            }
+        }, 1000)
     }
 }
 
-const styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-
-    }
-}
+const styles = {}
