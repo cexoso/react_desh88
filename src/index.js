dc3aedc10640136e46b588936e3a1606db3fc25f
熊杰 <285516193@qq.com>
2018-01-26 18:02:31 +0800

    router改写成标签的形式

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Route';
 import registerServiceWorker from './registerServiceWorker';
-import {I18nextProvider} from 'react-i18next';
+import { I18nextProvider } from 'react-i18next';
 import i18n from './service/I18n';
 
-ReactDOM.render(<I18nextProvider i18n={i18n}>{Routes()}</I18nextProvider>, document.getElementById('root'));
+ReactDOM.render(
+    <I18nextProvider i18n={i18n}>
+        <Routes />
+    </I18nextProvider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
